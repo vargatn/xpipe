6e270f26a829eeb0160b89e95981c11e94a2fb15
Tamas Norbert Varga <vargatn@gmail.com>
2018-03-19 16:24:26 +0100

    added kmeans_radec as dependency

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(name="xpipe",
       packages=find_packages(),
       description="Measurement and data processing for DES Y1 cluster lensing",
       install_requires=['numpy', 'scipy', 'pandas', 'astropy', 'fitsio', 'kmeans_radec', ],
+      dependency_links=['http://github.com/user/repo/tarball/master#egg=package-1.0'],
       author="Tamas Norbert Varga",
       author_email="T.Varga@usm.lmu.de",
       version="0.4")
+
