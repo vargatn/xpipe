544a3933e870bb0fd3bc1b838df484fae6f0eee2
Tamas Norbert Varga <vargatn@gmail.com>
2022-03-14 20:34:15 +0100

    clening up sequential submit script

--- bin/likelihood/submit_quintiles.sh
+++ bin/likelihood/submit_quintiles.sh
@@ -2,5 +2,13 @@
 for i in 0 1 2
 do
 #  screen -dm bash -c  "conda activate main3; sleep 10; python run_quintile.py --zbin=$i"
-  screen -dm bash -c "source activate main3; sleep 10; python run_quintile.py --zbin=$i"
+  screen -dm bash -c "
+                      source activate main3;
+                      sleep 10;
+                      python run_quintile.py --zbin=$i --fiducial --refs --no_overwrite;
+                      sleep 10;
+                      python run_quintile.py --zbin=$i --no_overwrite --exts;
+                      sleep 10;
+                      python run_quintile.py --zbin=$i --no_overwrite --effs;
+                      "
 done
