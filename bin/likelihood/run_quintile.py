7876166659da82e51cff63a5f1c261992d6ebebc
Tamas Norbert Varga <vargatn@gmail.com>
2022-03-14 16:36:34 +0100

    removed unused dependencies from script

--- bin/likelihood/run_quintile.py
+++ bin/likelihood/run_quintile.py
@@ -5,66 +5,19 @@
 
 """
 
-
 import pandas as pd
 import numpy as np
-import healpy as hp
-import h5py
-import matplotlib.pyplot as plt
-import yaml
-import fitsio as fio
 
-import copy
-
-from collections import OrderedDict
-
-import xpipe.tools.catalogs as catalogs
 import xpipe.paths as paths
 import xpipe.xhandle.parbins as parbins
-import xpipe.xhandle.xwrap as xwrap
-import xpipe.tools.selector as selector
-import xpipe.xhandle.shearops as shearops
-import matplotlib as mpl
 
-import astropy.constants as constants
-import astropy.units as u
 import astropy.cosmology as cosmology
 
-import scipy.interpolate as interp
-
-from importlib import reload
 import pickle
 
 import xpipe.tools.y3_sompz as sompz
-
-import xpipe.tools.selector as sl
-import xpipe.tools.visual as visual
-import xpipe.xhandle.pzboost as pzboost
-
-import sklearn
-import sklearn.covariance
-import sklearn.neighbors
-import sklearn.decomposition
-import scipy.stats as stats
-
-import NFW
-import scipy.optimize as optimize
-import emcee
-from cluster_toolkit import deltasigma
-
-from cluster_toolkit import xi
-from cluster_toolkit import bias
-
-
-from classy import Class
-from multiprocessing import Pool
-
-import corner
-import xpipe.likelihood.mass as mass
 import xpipe.likelihood.quintiles as quintiles
 
-import scipy.ndimage as ndimage
-import xpipe.tools.visual as visual
 import argparse
 
 cosmo = cosmology.FlatLambdaCDM(Om0=0.3, H0=70)
